1679e63eeeac41514c3bb3b736fce2088b91247e
hamflx <a@hamflx.cn>
2023-09-21 14:30:49 +0800

    docs: 更新使用说明。

--- examples/version/build.rs
+++ examples/version/build.rs
@@ -2,7 +2,4 @@ use forward_dll::forward_dll;
 
 fn main() {
     forward_dll("C:\\Windows\\system32\\version.dll").unwrap();
-
-    // 32 位目标。
-    // forward_dll("C:\\Windows\\SysWOW64\\version.dll").unwrap();
 }
